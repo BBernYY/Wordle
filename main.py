51407239039ce2edc21a607f1f8f34d42bed2e7b
BBernYY <66414852+BBernYY@users.noreply.github.com>
2022-09-16 19:53:53 +0200

    updated

--- main.py
+++ main.py
@@ -1,8 +1,58 @@
-def main():
-    import utils # imports my customized utils module, with a test and timing function. https://GitHub.com/BBernYY/FancyCoding
-    print("hello world!")
-
-
-
-if __name__ == '__main__': # checks if the code is ran as a file
-    main() # starts the main function
+import random # import random module
+inputwords = open('allowed_words.txt', 'r').read().split("\n") # download allowed words
+words = open("words.txt", "r").readlines() # download answer words
+word = random.choice(words).replace("\n", "").upper() # pick a word
+def checker(guess, answer): # return a list showing which letters are in the correct or wrong place, and if they are in the answer
+    result = [] # initialize result list
+    for i in range(5): # with each letter
+        if guess[i] == answer[i]:
+            result.append("V") # add check mark if necessary
+        elif guess[i] in answer:
+            result.append("~") # add tilda if necessary
+        else:
+            result.append("X") # add cross mark if necessary
+    return result # return the result
+guessing = True
+guesses = [
+    [" ", " ", " ", " ", " "],
+    [" ", " ", " ", " ", " "],
+    [" ", " ", " ", " ", " "],
+    [" ", " ", " ", " ", " "],
+    [" ", " ", " ", " ", " "],
+    [" ", " ", " ", " ", " "]
+]
+inputs = [
+    [" ", " ", " ", " ", " "],
+    [" ", " ", " ", " ", " "],
+    [" ", " ", " ", " ", " "],
+    [" ", " ", " ", " ", " "],
+    [" ", " ", " ", " ", " "],
+    [" ", " ", " ", " ", " "]
+]
+guessnum = 0
+while guessing: # gameloop
+    text = open("ASCII.txt", "r").read() # download the ascii text
+    for i in range(30): # fill in the values
+        text = text.replace(f"${str(i)}$", [k for j in inputs for k in j][i])
+    for i in range(30):
+        text = text.replace(f"$R{str(i)}$", [k for j in guesses for k in j][i])
+    print(text) # print output
+    insert = input("").upper()
+    while not insert in inputwords: # error message loop
+        print("SORRY I [DONT] SEEM TO [UNDERSTAND]\n\nTRY [AGAIN]:\n")
+        insert = input("").upper()
+    inputs[guessnum] = list(insert)
+    guess = checker(insert, word)
+    guesses[guessnum] = guess
+    if guess == checker("aaaaa", "aaaaa"): # if won
+        print("gg ez")
+        guessing = False
+        break
+    elif guessnum > 4: # if loss
+        print(f"YOU [FOOL] THE WORD WAS [{word}]")
+        guessing = False
+        break
+    guessnum += 1
+print("YOUR [SCORE] IS:\n\n")
+for i in guesses: # print score
+    print(i)
--- starter.bat
+++
@@ -1,5 +0,0 @@
-git init
-git add .
-git commit -m "Created Project"
-code .
-start /b "" cmd /c del "%~f0"&exit /b
